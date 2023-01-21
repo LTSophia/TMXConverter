5baad45d0f9b710f915516b1405565408efecd2b
Sophia Treadwell <sophia.treadwell@jackpinetech.com>
2023-01-21 00:06:49 -0500

    Added solidifying option using gmic.

--- TMXConverter/Program.cs
+++ TMXConverter/Program.cs
@@ -12,6 +12,7 @@ using System.Runtime.InteropServices;
 using System.Drawing.Imaging;
 using ImageFormat = Pfim.ImageFormat;
 using static System.Net.Mime.MediaTypeNames;
+using System.Diagnostics;
 
 namespace TMXConverter
 {
@@ -67,27 +68,58 @@ namespace TMXConverter
             if (args.Length == 0)
             {
                 Console.WriteLine("usage: TMXConverter imagefile(png, jpeg, bmp or tga)");
-                Console.WriteLine("       -o -output: sets name of file output");
-                Console.WriteLine("       -u -userid: sets user id of TMX");
-                Console.WriteLine("       -c -comment: sets user ");
+                Console.WriteLine("      -o -output: sets name of file output");
+                Console.WriteLine("      -u -userid: sets user id of TMX");
+                Console.WriteLine("      -c -comment: sets user ");
+                Console.WriteLine("      -s -solidify: solidifies edge of texture to remove haloing ");
             }
             else
             {
                 string input = args[0];
                 short userID = 1;
                 string comment = "";
+                bool solidify = false;
                 string output = Path.ChangeExtension(input, "tmx");
                 for (var i = 1; i < args.Length; i++)
                 {
                     if (args[i].ToLower() == "-userid" || args[i] == "-u") { userID = short.Parse(args[i + 1].Trim(trimChars)); }
                     else if (args[i].ToLower() == "-comment" || args[i] == "-c") { comment = args[i + 1].Trim(trimChars); }
                     else if (args[i].ToLower() == "-output" || args[i] == "-o") { output = args[i + 1].Trim(trimChars); }
+                    else if (args[i].ToLower() == "-solidify" || args[i] == "-s") { solidify = true; }
                 }
+                if(!Path.Exists(input)) { Console.WriteLine("Could not find file"); return; }
+                if(Path.GetDirectoryName(output) != "" && !Path.Exists(Path.GetDirectoryName(output))) { Console.WriteLine("Output folder does not exist"); return; }
 
                 if (comment.Length > 28) { comment = comment.Substring(0, 28); }
-
-                Bitmap image = (Path.GetExtension(input).ToLower() == ".tga") ? ToBitmap(Pfimage.FromFile(input)) : new Bitmap(input, true);
-                Color[] tmxbyte = new Color[(image.Width - (image.Width % 8) ) * ( image.Height - (image.Height % 2))];
+                Bitmap image;
+                if (solidify)
+                {
+                    ProcessStartInfo startInfo = new ProcessStartInfo();
+                    startInfo.FileName = "gmic\\gmic.exe";
+                    string newfile = Path.GetFileNameWithoutExtension(input) + "-Solidify.png";
+                    startInfo.Arguments = "\"" + input + "\" solidify , output " + newfile;
+                    using (Process exeProcess = Process.Start(startInfo))
+                    {
+                        exeProcess.WaitForExit();
+                    }
+                    image = (Path.GetExtension(input).ToLower() == ".tga") ? ToBitmap(Pfimage.FromFile(input)) : new Bitmap(input, true);
+                    Bitmap image2 = new Bitmap(newfile, true);
+                    Color c;
+                    for (int y = 0; y < image.Height; y += 1)
+                    {
+                        for (int x = 0; x < image.Width; x += 1)
+                        {
+                            c = image2.GetPixel(x, y);
+                            c = Color.FromArgb(image.GetPixel(x, y).A, c.R, c.G, c.B);
+                            image.SetPixel(x, y, c);
+                        }
+                    }
+                    image2.Dispose();
+                    File.Delete(newfile);
+                }
+                else { image = (Path.GetExtension(input).ToLower() == ".tga") ? ToBitmap(Pfimage.FromFile(input)) : new Bitmap(input, true); }
+                
+                Color[] tmxbyte = new Color[(image.Width - (image.Width % 8)) * (image.Height - (image.Height % 2))];
                 int num = 0;
 
                 for (int y = 0; y < image.Height - (image.Height % 2); y += 1)
@@ -126,9 +158,8 @@ namespace TMXConverter
                         bw.Write((byte)((tmxbyte[i].A * 128/255)));
 
                     }
-
-                }
                 
+                }
             }
             
         }
